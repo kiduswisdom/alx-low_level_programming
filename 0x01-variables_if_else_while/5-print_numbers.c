0fa7d970cc35f8b0fa23fce43c1731b2d69f35e1
kiduswisdom <kidusjob@gmail.com>
2022-07-01 17:46:31 -0700

    print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints the numbers since 0 to 9
- *     
+ *
  * Return: Always (Success)
  */
 int main(void)
@@ -11,10 +11,10 @@ int main(void)
 
 	for (n = '0'; n <= '9'; n++)
 	{
-	putchar(n);
+		putchar(n);
 	}
 
-	putchar('\n');
+		putchar('\n');
 
-	return (0);
+		return (0);
 }
