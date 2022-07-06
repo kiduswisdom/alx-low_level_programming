511a0aada7a44f4c1d6dd65c2e2c3921f1cbfa48
kiduswisdom <kidusjob@gmail.com>
2022-07-05 18:01:41 -0700

    debugging example

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void positive_or_negative(int i)
+{
+
+}
+
+/**
+ * main - debugging example
+ * Return: 0
+ */
+
+int main(void)
+{
+
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
