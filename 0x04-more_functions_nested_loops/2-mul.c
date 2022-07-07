fd2e2d859dba6ccda62781edde2c97f729101506
kiduswisdom <kidusjob@gmail.com>
2022-07-07 04:51:55 -0700

    multiplying two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* mul -> multiplying 2 numbers
+* @a: ingeter param1 
+* @b: integere param 2
+* Return: success and intiger
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
