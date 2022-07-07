45fad04b3ff56f3a197bcd6c1f00b5dcdf14c906
kiduswisdom <kidusjob@gmail.com>
2022-07-07 05:40:41 -0700

    prints more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers -> this prints most numbers
+ *
+ */
+void more_numbers(void)
+{
+int x, y;
+for (x = 0; x < 10; x++)
+{
+for (y = 0; y < 15; y++)
+{
+_putchar(x + '0');
+}
+_putchar('\n');
+}
+}
