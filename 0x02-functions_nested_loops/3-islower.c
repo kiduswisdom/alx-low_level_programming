c46f1b91a2ab3ee6e058ae7120966a1482ec307f
kiduswisdom <kidusjob@gmail.com>
2022-07-06 19:22:33 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * islower -> checks if the character is laowercase
+ * Return: returns 1 and 0 depending on condition
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z')
+}
