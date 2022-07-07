d91aac69f08f2ab62f0ceb6f9353e24dcc89ead7
kiduswisdom <kidusjob@gmail.com>
2022-07-06 17:54:49 -0700

    1-alphabet prints

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,11 +1,19 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
-* main - check the code for Holberton School students.
-* Return: Always 0.
+* main - Check description
+* Description: It prints the word Holberton, followed by a new line.
+* Return: 0.
 */
 int main(void)
 {
-print_alphabet();
+char word[9] = "main";
+int i;
+
+for (i = 0; i < 9; i++)
+_putchar(word[i]);
+_putchar('\n');
+
 return (0);
 }
