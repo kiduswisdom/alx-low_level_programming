0b93971762715ddc66cf73c1408dc9e98f1ba96e
kiduswisdom <kidusjob@gmail.com>
2022-07-07 02:10:02 -0700

    checking upper class letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper -> this is a function to check upper case alphas
+ * @c: an integer paramer
+ * Return: something but success
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+
+	return (c >= 65 && c < = 90);
+	return (c >= 'A' && c <= 'Z');
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is readme for this project of More function nest loops of C language 
