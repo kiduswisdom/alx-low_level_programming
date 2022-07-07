7de181048783223f832a131fa9de1f8be5730f20
kiduswisdom <kidusjob@gmail.com>
2022-07-06 19:34:31 -0700

    ilowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,23 @@
 #include "main.h"
 
 /**
- * islower -> checks if the character is laowercase
- * Return: returns 1 and 0 depending on condition
+ * _islower - Check Holberton
+ * @c: An input character
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
  */
 int _islower(int c)
 {
-	return (c >= 'a' && c <= 'z');
+	char i;
+	int lower = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+
+		if (i == c)
+
+		lower = 1;
+		}
+	return (lower);
 }
