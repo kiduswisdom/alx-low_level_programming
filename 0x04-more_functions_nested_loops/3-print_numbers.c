f257078a7189fb9d43550acc4e6d979b489adb6f
kiduswisdom <kidusjob@gmail.com>
2022-07-07 05:16:14 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,13 @@
 #include "main.h"
 
 /**
-* print_most_numbers -> this prints most numbers
+* print_numbers ->prints numbers
 *
 */
 void print_numbers(void)
 {
 int x;
 for (x = 0; x < 10; x++)
-{
-if (x != 2 && x != 4)
 _putchar(x + '0');
-}
 _putchar('\n');
 }
