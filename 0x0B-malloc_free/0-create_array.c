a8daf035751e11e14d230a65792d29742ae62681
kiduswisdom <kidusjob@gmail.com>
2022-07-25 12:36:29 -0700

    addedd

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,52 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to prin
- * @size: the size of the memory to print
- *
- * Return: Nothing
+ * malloc_checked - allocate memory with malloc
+ * @b: unsigned int typ
+ * Return: return pointe
  */
 
-void simple_print_buffer(char *buffer, unsigned int size)
+void *malloc_checked(unsigned int b)
 {
-	unsigned int i;
 
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
-	}
-	printf("\n");
-}
+	int *ptr;
 
-/**
- * main - check the code for ALX School students.
- * Return: Always 0.
- */
-
-int main(void)
-{
-	char *buffer;
-
-	buffer = create_array(98, 'H');
-	if  (buffer == NULL)
-	{
-		printf("failed to allocate memory\n");
-		return (1);
-	}
-	simple_print_buffer(buffer, 98);
-	free(buffer);
-	return (0);
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
 }
