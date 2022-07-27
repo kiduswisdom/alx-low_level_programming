6dd9d5365a0f538ddc380c67ea57ec475f9de26e
kiduswisdom <kidusjob@gmail.com>
2022-07-27 03:12:07 -0700

    hope

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 
-/*
- * ch_free_grid - frees a 2 dimensional array
- * @grid: multidimensional array of char
- * @height: height of the array.
- *
+/**
+ * ch_free_grid - frees a 2 dimensional array.
+ * @grid: multidimensional array of char.
+ * @height: height of the array
  * Return: no return
- */void ch_free_grid(char **grid, unsigned int height)
+ */
+void ch_free_grid(char **grid, unsigned int height)
 {
 	if (grid != NULL && height != 0)
 	{
@@ -17,7 +17,8 @@
 		free(grid);
 	}
 }
-/*
+
+/**
  * strtow - splits a string into words.
  * @str: string.
  * Return: pointer of an array of integers
@@ -39,13 +40,13 @@ char **strtow(char *str)
 	{
 		free(aout);
 		return (NULL);
-
+	}
 	for (i = a1 = 0; i < height; i++)
 	{
 		for (c = a1; str[c] != '\0'; c++)
 		{
 			if (str[c] == ' ')
-					a1++;
+				a1++;
 		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			{
 				aout[i] = malloc((c - a1 + 2) * sizeof(char));
@@ -54,6 +55,7 @@ char **strtow(char *str)
 					ch_free_grid(aout, i);
 					return (NULL);
 				}
+
 				break;
 			}
 		}
