919b614b98de3316debc001855c468e2d3bbaf9e
kiduswisdom <kidusjob@gmail.com>
2022-07-27 02:19:02 -0700

    alla

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,41 +1,34 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * _realloc - reallocate memory size function
- * @ptr: pointer to address of old memory location
- * @old_size: unsigned int type of old memory size
- * @new_size: unsigned int type for new memory size
- * Return:  return pointer to array
+ * _realloc - reallocates a memory block using malloc and fre
+ * @ptr: pointer to reallocate memor
+ * @old_size: size in bytes of allocated memor
+ * @new_size: newsize of memory block in byte
+ * Return: void pointer to new allocation of memory
  */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	char *p;
+	unsigned int i;
 
-	char *s;
-
-	if (new_size > old_size)
-	{
-		s = malloc(new_size);
-		free(ptr);
-		return (s);
-	}
-	if (new_size == old_size)
-	{
-		return (ptr);
-	}
 	if (ptr == NULL)
 	{
-		s = malloc(new_size);
-		free(ptr);
-		return (s);
+		p = malloc(new_size);
+		return (p);
 	}
-	if (new_size == 0 && ptr != NULL)
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	return (ptr);
+	if (old_size == new_size)
+		return (ptr);
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < old_size && i < new_size; i++)
+		p[i] = ((char *)ptr)[i];
+	free(ptr);
+	return (p);
 }
