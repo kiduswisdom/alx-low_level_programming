ba25c89d9a844122b228ce6b28c563ee2b61ac98
kiduswisdom <kidusjob@gmail.com>
2022-08-04 00:56:54 -0700

    3-get_op_func.c FIle - Updated

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -11,11 +11,10 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{ "+", op_add },	
+		{ "+", op_add },
 		{ "-", op_sub },
 		{ "*", op_mul },
-		{ "/", op_div 
-
+		{ "/", op_div }
 		{ "%", op_mod },
 			{ NULL, NULL }
 	};
@@ -24,7 +23,7 @@ int (*get_op_func(char *s))(int, int)
 	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);	
+			return (ops[i].f);
 		i++;
 	}
 
