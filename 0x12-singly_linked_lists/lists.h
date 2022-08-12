f9654d97868ef485b73e3f23021d31c9e3e7b914
kiduswisdom <kidusjob@gmail.com>
2022-08-11 21:58:29 -0700

    update-lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,6 @@
-#include <stdlib.h>
-#ifndef LISTS_H
-#define LISTS_H
-#include <stdlib.h>
-#include <string.h>
+#ifndef LINKLIST
+#define LINKLIST
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -14,11 +12,13 @@
  */
 typedef struct list_s
 {
-		char *str;
-			unsigned int len;
-				struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
+#include <stdlib.h>
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
