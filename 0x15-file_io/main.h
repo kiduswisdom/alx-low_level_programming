da5a768fba4d80bd00c98ebfc67078a51b02633a
kiduswisdom <kidusjob@gmail.com>
2022-08-24 02:37:41 -0700

    main.h-update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,24 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <elf.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <elf.h>
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
-void check_elf(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type, unsigned char *e_ident);
-void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf(int elf);
+void read_elf_header(int32_t fd, Elf32_Ehdr *elf_header);
+bool is_ELF(Elf32_Ehdr eh);
+void print_os(Elf32_Ehdr eh);
+void print_typ(Elf32_Ehdr eh);
+void print_clas(Elf32_Ehdr eh);
+void print_dta(Elf32_Ehdr eh);
+void print_vrsn(Elf32_Ehdr eh);
+void print_elf_header(Elf32_Ehdr elf_header);
 #endif
