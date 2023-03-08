351b5c360eea4aecf15c129c5c7f357b672bf075
Kidus Ataklti <kidusjob@gmail.com>
2023-03-08 01:51:31 -0800

    update

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,11 +1,8 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-/*prototypes*/
-
-int _putchar(char c);
+#ifndef _MAIN_H_
+#define _MAIN_H_
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
